9dc57023f0f22b0df5af62d86f74ed24b3294877
nostroguardian <shmelivan5@yandex.ru>
2024-02-28 18:20:59 +0300

    COMPLETED FIRST COMPONENT

--- dist/app.js
+++ dist/app.js
@@ -1127,7 +1127,20 @@
       this.el.classList.add('header');
       this.el.innerHTML = `
         <div>
-            <img src="/static/logo.svg" alt="Logo">
+          <img src="/static/logo.svg" alt="Logo">
+        </div>
+        <div class="menu">
+          <a class="menu__item" href="#">
+            <img src="/static/search.svg" alt="Search">
+            Поиск книг
+          </a>
+          <a class="menu__item" href="#favorites">
+            <img src="/static/favorites.svg" alt="Favorites">
+            Избранное
+            <div class="menu__counter">
+              ${this.appState.favorites.length}
+            </div>
+          </a>
         </div>
     `;
       return this.el;
